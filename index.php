4ffb197d9cee803c5a7d37c45b7ab7193fcdebef
NORMAN <normanshafiq012@gmail.com>
2024-08-23 08:45:35 +0800

    Tambah fail index.php yang pertama

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Latihan Git</title>
+</head>
+<body>
+<?php
+echo '<p>Selamat datang.</p>';
+echo '<p>Ini adalah fail yang pertama.</p>';
+?>
+</body>
+</html>
